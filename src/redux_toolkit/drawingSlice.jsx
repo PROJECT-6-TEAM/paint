a89b73513db5e23dbd9f7a16ae47568b62735db5
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-12 13:46:40 +0900

    feat : 선택한 색상을 전역스토어에 저장하는 기능 구현

--- src/redux_toolkit/drawingSlice.jsx
+++ src/redux_toolkit/drawingSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   dots: [],
   lines: [],
-  currentColor: "",
+  currentColor: "black",
 };
 
 const drawingSlice = createSlice({
