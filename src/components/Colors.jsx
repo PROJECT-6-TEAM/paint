d4b7ba21ed138fcfe80e6cf227cfd36f8ea02cf2
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-14 23:39:00 +0900

    style : 현재 색상 관리 컴포넌트 스타일 tailwind css로 변경

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import styles from "styles/colors.module.css";
 import { useDispatch } from "react-redux";
 import { currentColor } from "redux_toolkit/drawingSlice";
 
 export default function Colors() {
   const dispatch = useDispatch();
+
+  const btnCommonStyle =
+    "w-6 h-6 border border-solid border-black focus:border-2";
 
   const handleColor = (color) => {
     dispatch(currentColor(color));
@@ -12,46 +14,46 @@ export default function Colors() {
 
   return (
     <>
-      <div className={styles.color_box}>
+      <div className="grid grid-cols-5 grid-rows-2 gap-2.5">
         <button
           onClick={() => handleColor("black")}
-          className={`${styles.color} ${styles.black}`}
+          className={`bg-black ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("gray")}
-          className={`${styles.color} ${styles.gray}`}
+          className={`bg-gray-400 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("brown")}
-          className={`${styles.color} ${styles.brown}`}
+          className={`bg-orange-900 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("red")}
-          className={`${styles.color} ${styles.red}`}
+          className={`bg-red-600 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("yellow")}
-          className={`${styles.color} ${styles.yellow}`}
+          className={`bg-yellow-300 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("orange")}
-          className={`${styles.color} ${styles.orange}`}
+          className={`bg-orange-500 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("green")}
-          className={`${styles.color} ${styles.green}`}
+          className={`bg-green-600 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("blue")}
-          className={`${styles.color} ${styles.blue}`}
+          className={`bg-blue-700 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("purple")}
-          className={`${styles.color} ${styles.purple}`}
+          className={`bg-purple-600 ${btnCommonStyle}`}
         />
         <button
           onClick={() => handleColor("white")}
-          className={`${styles.color} ${styles.white}`}
+          className={`bg-white ${btnCommonStyle}`}
         />
       </div>
     </>
